d893c81f4ca00b4693371d4c8fe7714e6d0b5173
NikolayGrinin <nikg@live.ru>
2022-01-22 23:03:04 +0300

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
- terraform {
-   required_providers {
-      yandex = {
-          source = "yandex-cloud/yandex"
-          version = "0.69.0"
-      }
-   }
- }
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.69.0"
+    }
+  }
+}
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
@@ -19,46 +19,46 @@ resource "yandex_compute_instance" "app" {
   hostname = "reddir-app-${count.index + 1}"
   count    = var.instance_count
 
-resources {
-cores = 2
-core_fraction = 20
-memory = 2
-}
+  resources {
+    cores         = 2
+    core_fraction = 20
+    memory        = 2
+  }
 
-boot_disk {
-initialize_params {
-# Указать id образа созданного в предыдущем домашнем задании
-image_id = var.image_id
-}
-}
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашнем задании
+      image_id = var.image_id
+    }
+  }
 
-metadata = {
- ssh-keys = "ubuntu:${file(var.public_key_path)}"
-}
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
 
 
-network_interface {
-# Указан id подсети default-ru-central1-a
-subnet_id = var.subnet_id
-nat = true
-}
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
 
-connection {
-type = "ssh"
-host = self.network_interface.0.nat_ip_address
-user = "ubuntu"
-agent = false
-# путь до приватного ключа
-private_key = file(var.private_key_path)
-}
+  connection {
+    type  = "ssh"
+    host  = self.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
 
-provisioner "file" {
+  provisioner "file" {
     source      = "files/puma.service"
     destination = "/tmp/puma.service"
-}
+  }
 
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
     script = "files/deploy.sh"
-}
+  }
 
 }
