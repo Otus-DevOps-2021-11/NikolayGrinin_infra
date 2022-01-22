92d048ba2bc2698694b887f80bef0c2c7fb52364
NikolayGrinin <nikg@live.ru>
2022-01-23 00:12:10 +0300

    update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.35"
-    }
-
+       source  = "yandex-cloud/yandex"
+        version = "~> 0.35"
   }
+
+}
 }
