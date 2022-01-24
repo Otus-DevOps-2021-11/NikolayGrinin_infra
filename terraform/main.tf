94209ca23b8b28e8aca355e0bbfe73b2f1faf4c2
NikolayGrinin <96079204+NikolayGrinin@users.noreply.github.com>
2022-01-24 20:17:54 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 
 provider "yandex" {
-  version = "~> 0.35"
+  version = ">= 0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
