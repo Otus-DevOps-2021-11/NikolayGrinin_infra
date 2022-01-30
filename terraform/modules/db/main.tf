efbec2098ec7c355ca714fca6a2f32d27335060b
NikolayGrinin <nikg@live.ru>
2022-01-30 23:21:52 +0300

    hw11

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,13 +1,13 @@
 
-#terraform {
-#  required_version = ">= 0.12"
-#  required_providers {
-#    yandex = {
-#           source = "yandex-cloud/yandex"
-#           version = ">= 0.35"
-#    }
-# }
-#}
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    yandex = {
+           source = "yandex-cloud/yandex"
+           version = "0.70.0"
+    }
+ }
+}
 
 
 
